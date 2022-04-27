c9d5b151425ebb9916871fad9e77d04a07205f36
vikkyvikas-4444 <88304799+vikkyvikas-4444@users.noreply.github.com>
2022-04-27 04:22:03 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function preload()
+{
+    ml5.imageClassifier('DoodleNet');
+}
+
+
+function setup()
+{
+    canvas=createCanvas(280 , 280);
+    canvas.center();
+    canvas.mouseReleased(classifyCanvas);
+    synth= window.speechSynthesis;
+    background("white");
+
+}
+
+function clearCanvas()
+{
+    background("white");
+    
+}
+
+function draw()
+{
+    strokeWeight(13);
+    stroke(0);
+
+    if(mouseIsPressed)
+    {
+        line(pmouseX , pmouseY , mouseX , mouseY);
+    }
+}
+
+function classifyCanvas()
+{
+    classifier.classify(canvas , gotResult);
+}
+
+function gotResult(error , results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+
+    console.log(results);
+document.getElementById('label').innerHTML='label:' + results[0].label;
+document.getElementById('confidence').innerHTML='confidence:' + Math.round(results[0].confidence * 100) + '%';
+ utterThis=new SpeechSynthesisUtterance(results[0].label);
+ synth.speak(utterThis);
+}
